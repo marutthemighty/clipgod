42efdfbf6e0f2b561cf9ee26aa38e010a67bbbe3
Marut The Mighty <marutthemighty@protonmail.com>
2025-12-12 15:53:01 +0530

    Create page.tsx -> May need more upgrades

---
+++ clipgod-frontend/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from '@/components/Hero';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <Hero />
+    </div>
+  );
+}
