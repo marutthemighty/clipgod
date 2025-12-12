b453f81f05f15e7f1805e244d42043b8cc207e48
Marut The Mighty <marutthemighty@protonmail.com>
2025-12-12 16:21:27 +0530

    Create tailwind.config.ts -> May need update later

---
+++ clipgod-frontend/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: { extend: {} },
+  plugins: [require('tailwindcss-animate')],
+  darkMode: 'class',
+};
+export default config;
