51036babe7ff62fc5d2c8f53b432d4a8991449f3
Marut The Mighty <marutthemighty@protonmail.com>
2025-12-12 16:18:11 +0530

    Create Leaderboard.tsx -> This file is incomplete

---
+++ clipgod-frontend/components/Leaderboard.tsx
@@ -0,0 +1,5 @@
+// For /app/leaderboard if added, but integrated in dashboard or separate
+export const Leaderboard = () => {
+  // Fetch top 10, show user rank
+  return <div>Top 10: ...</div>;
+};
